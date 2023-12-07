d7399ae8dd3feb3d4464954f2950b6b6943fdf5d
JasonHoang1 <146388795+JasonHoang1@users.noreply.github.com>
2023-12-06 20:51:36 -0800

    Update main.java

--- main.java
+++ main.java
@@ -7,12 +7,14 @@ public class AppClient {
     private Scanner scan;
     private ArrayList<Appliance> appliances = new ArrayList<>();
 
+    // Method to read appliance details from a file
     public void readAppFile(String file) {
         try {
             File myFile = new File(file);
             scan = new Scanner(myFile);
 
             while (scan.hasNextLine()) {
+                // Split the line into individual details using ","
                 String[] applianceDetails = scan.nextLine().split(",");
                 int locationID = Integer.parseInt(applianceDetails[0]);
                 String appName = applianceDetails[1];
@@ -21,6 +23,7 @@ public class AppClient {
                 String appType = applianceDetails[4];
                 int lowPower = Integer.parseInt(applianceDetails[5]);
 
+                // Create a new Appliance object and add it to the list
                 Appliance aAppl = new Appliance(locationID, appName, onPower, probOn, appType, lowPower);
                 appliances.add(aAppl);
             }
@@ -30,8 +33,8 @@ public class AppClient {
         }
     }
 
+    // Method to add a new appliance
     public void addAppliance() {
-        // Implement logic to add an appliance
         System.out.println("Enter location ID:");
         int locationID = Integer.parseInt(scan.nextLine());
 
@@ -50,37 +53,78 @@ public class AppClient {
         System.out.println("Enter power reduction percent when changed to low status (floating point, e.g., 0.33 for 33% reduction):");
         float probSmart = isSmart ? Float.parseFloat(scan.nextLine()) : 0.0f;
 
+        // Create a new Appliance object and add it to the list
         Appliance newAppliance = new Appliance(locationID, appName, onPower, probOn, isSmart, probSmart);
         appliances.add(newAppliance);
         System.out.println("Appliance added successfully.");
     }
 
+    // Method to delete an existing appliance
     public void deleteAppliance() {
-        // Implement logic to delete an appliance
         System.out.println("Enter the name of the appliance to delete:");
         String applianceName = scan.nextLine();
 
+        // Remove the appliance with the specified name from the list
         appliances.removeIf(appliance -> appliance.getName().equals(applianceName));
         System.out.println("Appliance deleted successfully.");
     }
 
+    // Method to list all existing appliances
     public void listAppliances() {
-        // Implement logic to list all appliances
+        System.out.println("List of Appliances:");
         for (Appliance appliance : appliances) {
             System.out.println(appliance.toString());
         }
+        System.out.println("End of List");
     }
 
-    public void startSimulation() {
+    // Method to start the simulation
+    public void startSimulation(double totalAllowedWattage, String filePath, int simulationLength) {
         // Placeholder for simulation logic
         System.out.println("Simulation is starting. Implement your simulation logic here.");
+        System.out.println("Total Allowed Wattage: " + totalAllowedWattage);
+        System.out.println("File Path: " + filePath);
+        System.out.println("Simulation Length: " + simulationLength);
     }
 
+    // Main method
     public static void main(String[] args) {
         AppClient app = new AppClient();
         app.scan = new Scanner(System.in);
 
+        // Prompt for total allowed wattage
+        System.out.println("Enter the total allowed wattage (power):");
+        double totalAllowedWattage = 0.0;
+        boolean validTotalWattage = false;
+        while (!validTotalWattage) {
+            try {
+                totalAllowedWattage = Double.parseDouble(app.scan.nextLine());
+                validTotalWattage = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number for total allowed wattage.");
+            }
+        }
+
+        // Prompt for comma-separated text file containing appliances
+        System.out.println("Enter the path of the comma-separated text file containing the appliances:");
+        String filePath = app.scan.nextLine();
+
+        // Prompt for simulation length in timesteps
+        System.out.println("Enter the simulation length in timesteps:");
+        int simulationLength = 0;
+        boolean validSimulationLength = false;
+        while (!validSimulationLength) {
+            try {
+                simulationLength = Integer.parseInt(app.scan.nextLine());
+                validSimulationLength = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer for simulation length.");
+            }
+        }
+
+        // Main program loop
         while (true) {
+            // Display menu
             System.out.println("Select an option:");
             System.out.println("Type \"A\" Add an appliance");
             System.out.println("Type \"D\" Delete an appliance");
@@ -89,8 +133,10 @@ public class AppClient {
             System.out.println("Type \"S\" To Start the simulation");
             System.out.println("Type \"Q\" Quit the program");
 
+            // Get user input
             String option1 = app.scan.nextLine();
 
+            // Perform actions based on user input
             switch (option1.toUpperCase()) {
                 case "A":
                     app.addAppliance();
@@ -102,10 +148,10 @@ public class AppClient {
                     app.listAppliances();
                     break;
                 case "F":
-                    app.readAppFile("your_file_path"); // Replace with your actual file path
+                    app.readAppFile(filePath);
                     break;
                 case "S":
-                    app.startSimulation();
+                    app.startSimulation(totalAllowedWattage, filePath, simulationLength);
                     break;
                 case "Q":
                     app.scan.close();
